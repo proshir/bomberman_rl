33ea4dde0471fdfda10d72800581870495c1fe96
Sahand Alitanloo <salitanl@hcigpu07>
2026-09-18 19:05:58 +0200

    Revert "add masked combat Double DQN alongside FQI"
    This reverts commit 3531cf52858b0445e22ad4ba1f75896d69c7d136.

--- agent_code/combat_dqn_agent/__init__.py
+++
@@ -1,3 +0,0 @@
-"""PyTorch Double-DQN combat agent using the audited combat representation."""
-
-# Sahand was here.
--- agent_code/combat_dqn_agent/features.py
+++
@@ -1,9 +0,0 @@
-"""Reuse the validated 32-feature history/anti-stagnation representation."""
-
-# Sahand was here.
-
-from agent_code.combat_fqi_history_antistag_agent.features import (  # noqa: F401
-    ACTIONS,
-    HISTORY_LENGTH,
-    state_to_features,
-)
